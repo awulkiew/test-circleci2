1c382a589587248aa80d988c7b22ee28d51ca95a
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-07-17 23:27:36 +0200

    First

---
+++ test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void foo()
+{
+    std::cout << "foo" << std::endl;
+}
+
+void bar()
+{
+    std::cout << "bar" << std::endl;
+}
+
+int main()
+{
+    std::cout << "Test 1" << std::endl;
+    foo();
+}
---
+++ test3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Test 3" << std::endl;
+}
